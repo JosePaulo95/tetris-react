6764c057a4c910fe687c26a4a059b0b0007f7c8d
jose.paulo <jose.paulo@ceuma.br>
2023-01-08 22:00:48 -0300

    change empty/void block idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import './App.css';
 
 import BlockGroup from './components/BlockGroup';
+import EmptyBlock from './components/EmptyBlock';
 
 function App() {
   const shape: number[][] = [
     [1, 0],
     [1, 1],
   ];
+  const empty_board: number[][] = [
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+  ];
   return (
     <>
-      <BlockGroup shape={shape}></BlockGroup>
-      <BlockGroup shape={shape}></BlockGroup>
+      {/* <BlockGroup shape={shape} Block={Color1Block}></BlockGroup> */}
+      <BlockGroup shape={empty_board} Block={EmptyBlock}></BlockGroup>
     </>
   );
 }
--- src/components/BlockGroup.tsx
+++ src/components/BlockGroup.tsx
@@ -2,17 +2,21 @@ import styles from '../styles/blocks.module.css';
 
 interface BlockGroupProps {
   shape: number[][];
-  EmptyBlock?: any;
+  Block?: any;
 }
 
-const BlockGroup = ({ shape, EmptyBlock }: BlockGroupProps) => {
+const BlockGroup = ({ shape, Block }: BlockGroupProps) => {
   return (
     <table>
       <tbody className={styles.BlockGroup}>
         {shape.map((row, i) => (
           <tr key={i}>
             {row.map((block, j) =>
-              block ? <td key={j} className={styles.block}></td> : EmptyBlock(),
+              block ? (
+                Block()
+              ) : (
+                <td key={j} className={`${styles.void_block} ${styles.block}`}></td>
+              ),
             )}
           </tr>
         ))}
