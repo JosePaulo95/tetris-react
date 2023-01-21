3599952358c1a65d18fd1bd92f706718837f03cb
jose.paulo <jose.paulo@ceuma.br>
2023-01-21 12:17:17 -0300

    checking ground collision

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import BlockGroup from './components/BlockGroup';
 import BoardContainer from './components/BoardContainer';
-import { transform } from './utils';
+import { checksCollision, transform, wrap } from './utils';
 
 function App() {
   const [falling_piece, setFallingPiece]: [
@@ -18,17 +18,29 @@ function App() {
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
   ]);
+  const outer_walls: number[][] = [
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+  ];
   const current_board: number[][] = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 2],
-    [0, 0, 3, 0, 1],
-    [0, 2, 2, 0, 1],
-    [1, 1, 2, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 2, 0, 0, 1],
+    [0, 0, 0, 0, 1],
   ];
   const empty_board: number[][] = [
     [-1, -1, -1, -1, -1],
@@ -42,10 +54,16 @@ function App() {
   ];
 
   useEffect(() => {
-    setInterval(() => {
-      setFallingPiece((falling_piece) => transform(falling_piece, 0, 1));
-    }, 1000);
-  }, []);
+    const interval = setInterval(() => {
+      const wrapped_piece = wrap(falling_piece);
+      const pos_move = transform(wrapped_piece, 0, 1);
+      const colliding = checksCollision(pos_move, outer_walls);
+      if (!colliding) {
+        setFallingPiece((falling_piece) => transform(falling_piece, 0, 1));
+      }
+    }, 100);
+    return () => clearInterval(interval);
+  }, [falling_piece]);
   return (
     <BoardContainer>
       <BlockGroup grid={falling_piece}></BlockGroup>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,3 +18,40 @@ export const transform = (board: number[][], x: number, y: number) => {
 
   return b;
 };
+
+export const wrap = (board: number[][]) => {
+  const wrapped: number[][] = [];
+  const width = board.length;
+  const height = board[0] ? board[0].length : 0;
+
+  for (let i = 0; i < width + 2; i++) {
+    wrapped.push([]);
+    for (let j = 0; j < height + 2; j++) {
+      wrapped[i].push(0);
+    }
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      wrapped[i + 1][j + 1] = board[i][j];
+    }
+  }
+
+  return wrapped;
+};
+
+export const checksCollision = (boardA: number[][], boardB: number[][]) => {
+  if (boardA.length != boardB.length || boardA[0]?.length != boardB[0]?.length) {
+    throw new Error('Comparing boards with different sizes is not allowed.');
+  }
+
+  for (let i = 0; i < boardA.length; i++) {
+    for (let j = 0; j < boardA[i].length; j++) {
+      if (boardA[i][j] > 0 && boardB[i][j] > 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
