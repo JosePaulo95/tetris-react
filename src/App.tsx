7b24013f885434c19d0b8cdfc0344d76d7683988
jose.paulo <jose.paulo@ceuma.br>
2023-01-21 19:31:52 -0300

    some renames

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,19 @@ import { randomPiece } from './factories/PieceFactory';
 import { isColliding, join, removeMatches, transform, wrap } from './utils';
 
 function App() {
-  const [falling_piece, setFallingPiece]: [
-    number[][],
-    Dispatch<SetStateAction<number[][]>>,
-  ] = useState(randomPiece());
-  const outer_walls: number[][] = [
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
+  const [piece, setPiece]: [number[][], Dispatch<SetStateAction<number[][]>>] = useState(
+    randomPiece(),
+  );
+  const outer_bottom: number[][] = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 1],
   ];
   const [current_board, setCurrentBoard]: [
@@ -60,16 +59,16 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const wrapped_piece = wrap(falling_piece);
-      const wrapped_pos_move = transform(wrapped_piece, 0, 1);
-      const ground_colliding = isColliding(wrapped_pos_move, outer_walls);
-      const pos_move = transform(falling_piece, 0, 1);
-      const current_board_colliding = isColliding(pos_move, current_board);
+      const wrapped_piece = wrap(piece);
+      const wrapped_pos_down_move = transform(wrapped_piece, 0, 1);
+      const piece_pos_down_move = transform(piece, 0, 1);
+      const will_ground_collide = isColliding(wrapped_pos_down_move, outer_bottom);
+      const will_board_collide = isColliding(piece_pos_down_move, current_board);
 
-      if (ground_colliding || current_board_colliding) {
-        const post_move = join(current_board, falling_piece);
-        const post_matches = removeMatches(post_move);
-        const game_over = isColliding(post_matches, outer_top);
+      if (will_ground_collide || will_board_collide) {
+        const board_plus_piece = join(current_board, piece);
+        const board_after_matches = removeMatches(board_plus_piece);
+        const game_over = isColliding(board_after_matches, outer_top);
 
         if (game_over) {
           setCurrentBoard([
@@ -83,18 +82,18 @@ function App() {
             [0, 0, 0, 0, 0],
           ]);
         } else {
-          setCurrentBoard(post_matches);
+          setCurrentBoard(board_after_matches);
         }
-        setFallingPiece(randomPiece());
+        setPiece(randomPiece());
       } else {
-        setFallingPiece(pos_move);
+        setPiece(piece_pos_down_move);
       }
-    }, 100);
+    }, 50);
     return () => clearInterval(interval);
-  }, [falling_piece]);
+  }, [piece]);
   return (
     <BoardContainer>
-      <BlockGroup grid={falling_piece}></BlockGroup>
+      <BlockGroup grid={piece}></BlockGroup>
       <BlockGroup grid={current_board}></BlockGroup>
       <BlockGroup grid={empty_board}></BlockGroup>
     </BoardContainer>
