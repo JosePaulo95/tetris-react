62ce785bc199fb06b1446792bbf60962cf2130c3
jose.paulo <jose.paulo@ceuma.br>
2023-08-26 11:42:30 -0300

    instala router e bota o jogo na raiz

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import store from './store';
@@ -10,7 +9,12 @@ import store from './store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <Router>
+        <Routes>
+          <Route path="/" Component={App} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
