e4a1c16f43595f0c18f7a382f1761778197a22af
Éverton Toffanetto <evertondgn@hotmail.com>
2023-10-15 20:39:41 -0300

    chore: config environments

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => ({
-  base: `${process.env.VITE_PUBLIC_PATH}/`,
-  plugins: [tsconfigPaths(), react()],
-  server: {
-    open: true,
-  },
-  test: {
-    environment: 'happy-dom',
-    globals: true,
-    passWithNoTests: true,
-    setupFiles: ['./vitest.setup.ts'],
-  },
-}));
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    base: `${process.env.VITE_PUBLIC_PATH}/`,
+    build: {
+      sourcemap: mode === 'development',
+    },
+    plugins: [tsconfigPaths(), react()],
+    server: {
+      open: true,
+      port: Number(process.env.VITE_PORT),
+    },
+    test: {
+      environment: 'happy-dom',
+      globals: true,
+      passWithNoTests: true,
+      setupFiles: ['./vitest.setup.ts'],
+    },
+  };
+});
