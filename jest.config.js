4cca40e5d0655c849fbe684a47242957854c64ad
Éverton Toffanetto <evertondgn@hotmail.com>
2023-10-15 19:58:24 -0300

    chore: remove jest config

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest', // para JavaScript e JSX
-    '^.+\\.tsx?$': 'ts-jest', // para TypeScript e TSX
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
